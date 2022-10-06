9d3b891049fa580b119611fa7c2bff8d62f2ffe8
sharropp <98764252+sharropp@users.noreply.github.com>
2022-10-05 21:56:11 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,18 +1,28 @@
 /**
  * Author:    Squantio
- * Created:   10.03.2022
+ * Created:   10.05.2022
  *
  * (c) Copyright by scrabungus Corp.
  **/
 
 
-      document.addEventListener(‘load’, () => {
-
+      document.addEventListener("DOMContentLoaded", () => {
         let btn1 = document.querySelector('#btn1');
-            let handler = () => {
+            let handler1 = () => {
               console.log('you clicked!');
               btn1.style.backgroundColor = 'purple';
               btn1.style.color = 'white';
+          };
+
+          btn1.addEventListener('click', handler1);
+
+          let btn2 = document.querySelector('#btn2');
+              let handler2 = () => {
+                console.log('you clicked!');
+                btn2.style.backgroundColor = 'red';
+                btn2.style.color = 'white';
             };
+
+            btn2.addEventListener('click', handler2);
 
       });
