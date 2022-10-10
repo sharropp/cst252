fa8d51c8541313fbec5734cc21f2773b5ce4b751
sharropp <98764252+sharropp@users.noreply.github.com>
2022-10-10 15:39:20 -0700

    the fix

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,16 +5,18 @@
  * (c) Copyright by scrabungus Corp.
  **/
 
+//pseudocode!
+
 //event 1 ^
 //hover mouse on itembox
 //border edge gets bolder
 
-//event 2
+//event 2 ^
 //hover over an itembox
 //click on itembox
 //panel on right will show up with a basic description of the course and a picture
 
-//event 3
+//event 3 ^
 //hover over an itembox
 //click on itembox
 //panel on right will show up with a basic description of the course and a picture
@@ -24,13 +26,14 @@
 //when the page loads the panel on the right displaying the course info is hidden
 
 document.addEventListener("DOMContentLoaded", () => {
+
 //to hide the infobox
 
 let infopanel = document.getElementById('infopanel');
 window.onload = function() {
-  console.log ("test")
     document.getElementById("infopanel").style.display = "none";
   }
+
   //event that makes the border of an item box turn thick when hovered on
 
 let itembox1 = document.getElementById('itembox1');
@@ -38,7 +41,6 @@ itembox1.onmouseout = function() {
     document.getElementById("itembox1").style.borderWidth = ("thin");
   }
   itembox1.onmouseover = function() {
-    console.log ("test")
       document.getElementById("itembox1").style.borderWidth = ("thick");
     }
 
@@ -47,25 +49,38 @@ itembox1.onmouseout = function() {
         document.getElementById("itembox2").style.borderWidth = ("thin");
       }
       itembox2.onmouseover = function() {
-        console.log ("test")
           document.getElementById("itembox2").style.borderWidth = ("thick");
         }
     //event that makes the infobox appear w/ different image depending on the button
 
         let showInfopanel1 = document.getElementById("itembox1").onclick = function() {
-          console.log('you clicked!');
+          console.log("test 1")
           document.getElementById("courseimage").src = "images/cst252.jpg";
           document.getElementById("infopanel").style.display = "block";
+          document.getElementById("cst252coursename").textContent = ('CST252:');
+          document.getElementById("cst252description").textContent = ('This class teaches you the basics of JavaScript!');
+          document.getElementById("cst351coursename").style.display = "none";
+          document.getElementById("cst351description").style.display = "none";
+          document.getElementById("cst252coursename").style.display = "block";
+          document.getElementById("cst252description").style.display = "block";
+
         };
 
         let showInfopanel2 = document.getElementById("itembox2").onclick = function() {
-          console.log('you clicked!');
+          console.log("test 2")
           document.getElementById("courseimage").src = "images/cst351.jpg";
           document.getElementById("infopanel").style.display = "block";
+          document.getElementById("cst351coursename").textContent = ('CST351:');
+          document.getElementById("cst351description").textContent = ('This class teaches you Web Design!');
+          document.getElementById("cst252coursename").style.display = "none";
+          document.getElementById("cst252description").style.display = "none";
+          document.getElementById("cst351coursename").style.display = "block";
+          document.getElementById("cst351description").style.display = "block";
         };
-    //event that closes info infopanel
+
+    //event that onclick closes info infopanel
+
     let infoClose = document.getElementById("exitcontainer").onclick = function() {
-      console.log('you clicked!');
       document.getElementById("infopanel").style.display = "none";
     };
 });
