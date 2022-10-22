29676a0d96634ee3e66a3cacf326134a1e6b28e8
sharropp <98764252+sharropp@users.noreply.github.com>
2022-10-21 21:08:28 -0700

    the lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let boxes = document.querySelectorAll(".box")
+  const boxes = document.querySelectorAll(".box");
   console.log(boxes);
-  let turnYellow = (e) => {
-    console.log(e.style.backgroundColor)
-    
-  }
 window.onload = function() {
-   console.log('It loaded when the page loaded!')
-   boxes.forEach(turnYellow)
+   console.log('It loaded when the page loaded!');
+   boxes.forEach(boxes => {
+   boxes.style.backgroundColor = "yellow";
+ });
+}
+  var boxes2 = document.getElementsByClassName('box')
+  for (var i = 0; i < boxes2.length; i++) {
+    // Here we have the same onclick
+    boxes2[i].addEventListener('click', function (event) {
+      console.log('Element ' + event.target.innerHTML + ' was just clicked')
+      this.style.borderWidth = ("thick");
+    })
   }
   });
