30169f13ea7b1b61498c7c466970c120a826eea6
sharropp <98764252+sharropp@users.noreply.github.com>
2022-12-04 18:22:27 -0800

    yrs

--- interstellar hackers/js/game.js
+++ interstellar hackers/js/game.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let textForm = document.querySelector('.textForm');
 //wordbank
   var wordBank = [
-    "millennium", "deathstar","asteroid","comet","galaxy","meteor","moon","saturn",
+    "millenium", "deathstar","asteroid","comet","galaxy","meteor","moon","saturn",
     "consellation","eclipse","orbit","satellite","space","astronaut","observatory",
     "solstice","astronomy","telescope","spaceship","orbit","mars","mercury",
   ];
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Function that submits the values into console
   textForm.addEventListener('click', function(event) {
-    console.log('codeworked')
     event.preventDefault();
 
     var counter = 10;
@@ -44,17 +43,42 @@ document.addEventListener('DOMContentLoaded', () => {
     randomWord = getRandomWord(wordBank);
     console.log('randomWord', randomWord);
   })
+
+  //making "_" equal to length of randomized word
+  let underscore = randomWord.length;
+  let result = randomWord.replace(/.?/g,"_ ");
+  document.getElementById("guess").innerHTML=result;
+
+  //replacing "_" with the correct letters guessed
+  //if input = correct find spot in array that has the correct letters
+  //then unhide correctly guessedletter which hides underscore
+
+  //problems: variable guessWord keeps saying that it is undefined, and consolelog doesnt want to work
+if (guessWord != null) {
+  if(/^[a-zA-Z]/.test(guessWord) && guessWord.length == 1) {
+  checkLetter();
+console.log('test');
+  }
+
+  else{
+      console.log("You need to put one letter");
+  }
+  function replaceAt(str, index, replacement) {
+  return (
+    str.substr(0, index) + replacement + str.substr(index + replacement.length)
+  );
+  }
+  function checkLetter() {
+  let matchedLetters = randomWord.match(guessWord);
+  return replaceAt(result, matchedLetters.index, guessWord);
+  }
+}
+
+
 });
 
 
-// "and", "a", "to", "in", "is", "you", "that", "it", "he",
-// "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be",
-// "this", "have", "from", "or", "one", "had", "by", "word", "but", "not",
-// "what", "all", "were", "we", "when", "your", "can", "said", "there",
-// "use", "an", "each", "which", "she", "do", "how", "their", "if", "will",
-// "up", "other", "about", "out", "many", "then", "them", "these", "so",
-// "some", "her", "would", "make", "like", "him", "into", "time", "has",
-// "look", "two", "more", "write", "go", "see", "number", "no", "way",
-// "could", "people", "my", "than", "first", "water", "been", "call",
-// "who", "oil", "its", "now", "find", "long", "down", "day", "did", "get",
-// "come", "made", "may", "part"
+//make side div
+//count legth of word
+//make ____ equal to length of word
+//once it hits letter it puts correct letter in correct spot
